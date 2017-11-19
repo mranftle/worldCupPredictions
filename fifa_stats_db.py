6c4fc0a5bf86064c327e59dc8e54028fc0ff63b2
mranftle <matthew.ranftle@gmail.com>
2017-11-18 23:43:29 -0600

    plot of team player ratings

--- fifa_stats_db.py
+++ fifa_stats_db.py
@@ -1,5 +1,11 @@
 import pandas as pd
+import numpy as np
+import random
 import string,os, re, math
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import yaml
 printable = set(string.printable)
 fifa_dataset = pd.read_csv("data/fifa-18-demo-player-dataset/CompleteDataset.csv")
 # player_attribute_data = pd.read_csv("data/fifa-18-demo-player-dataset/PlayerAttributeData.csv")
@@ -26,12 +32,12 @@ def get_player_rating(country,roster):
     # print fifa_players.loc[fifa_players['Name'].isin()]
     players_with_ranks = pd.DataFrame(columns=['Name', 'Nationality', 'Overall'])
     for name in roster:
-        print name
+        # print name
         p = fifa_players.loc[fifa_players['Name'] == name]
         if not p.empty:
             # print type(p)
             players_with_ranks = players_with_ranks.append(p)
-    return players_with_ranks
+    return players_with_ranks.groupby(['Name']).max()
 
 def average_player_ratings():
     # get team rosters
@@ -43,17 +49,43 @@ def average_player_ratings():
 
     rosters = {k: get_player_rating(k.title(),v) for k,v in rosters.items()}
 
-    #Get average player rating for each country
-    average_player_ratings = {}
-    for country in rosters:
-        max_player_ratings = rosters[country].groupby(['Name']).max()
-        average_player_rating = max_player_ratings['Overall'].mean()
-        if math.isnan(average_player_rating):
-            average_player_rating = 0
-        average_player_ratings[country] = average_player_rating
+    plot_with_plotly(rosters)
 
-    for key, value in sorted(average_player_ratings.iteritems(), key=lambda (k,v): (v,k)):
-        print "%s: %s" % (key, value)
+
+        # '''read in yml configs'''
+        # with open('plotly_config.yaml', 'r') as f:
+        #     doc = yaml.load(f)
+        # username = doc['username']
+        # password = doc['password']
+        # py.sign_in(username, password)
+
+        # average_player_rating = max_player_ratings['Overall'].mean()
+        # if math.isnan(average_player_rating):
+        #     average_player_rating = 0
+        # average_player_ratings[country] = average_player_rating
+
+    # for key, value in sorted(average_player_ratings.iteritems(), key=lambda (k,v): (v,k)):
+    #     print "%s: %s" % (key, value)
+    #
+    # plot_with_plotly(average_player_ratings)
+
+def plot_with_plotly(roster):
+
+    #     ratings = list(roster[team]['Overall'])
+    #     temp_trace = go.Box(
+    #         y = ratings,
+    #         name = team.title()
+    #     )
+    #     traces.append(temp_trace)
+    # py.iplot(traces)
+
+
+    # N = len(roster)
+    # c = ['hsl(' + str(h) + ',50%' + ',50%)' for h in np.linspace(0, 360, N)]
+    #
+    # data = [{
+    #     'y':
+    # }]
 
 def main():
     average_player_ratings()
